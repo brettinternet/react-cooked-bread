e278bcb6a1bf36aeb010f938a7c0e3849859cb18
brett <brettinternet@gmail.com>
2020-08-05 23:34:54 -0600

    feat: add maxToasts, pauseOnFocusLoss, reverseColumn
    also rename default prop names to avoid confusion; separate content and root styles; fix recursion
    issue with custom toast id
    
    BREAKING CHANGE: Default props applied to toasts and passed to the provider have been renamed to
    match the prop option provided to toast context props. Toast root and content styles have been
    separated into two separate props passed to the provider component. `addToast` now returns a string
    that is always defined. Toast options for toast creation and updates have properties that have been
    renamed and some props have been added to support some custom toast content components.
    `TransitionDuration` has been added as a toast option to override the Transition Group timeout prop.

--- packages/react-cooked-bread/src/active-toasts-hook.ts
+++ packages/react-cooked-bread/src/active-toasts-hook.ts
@@ -3,17 +3,17 @@ import { useState, useCallback } from 'react'
 import { ActiveToast, Id, AddToastOptions, UpdateToastOptions } from './types'
 import { getId } from './utils'
 
-export const useActiveToasts = () => {
+export const useActiveToasts = (maxToasts: number | undefined) => {
   const [toasts, setToasts] = useState<ActiveToast[]>([])
   const hasToasts = !!toasts.length
 
-  const exists = (id: Id | undefined) => {
+  const exists = (id: Id) => {
     if (id && hasToasts) {
       return !!toasts.filter((t) => t.id === id).length
     }
   }
 
-  const removeToast = (id: Id | undefined) => {
+  const removeToast = (id: Id) => {
     if (exists(id)) {
       setToasts((prevToasts) => prevToasts.filter((t) => t.id !== id))
     }
@@ -23,15 +23,24 @@ export const useActiveToasts = () => {
     setToasts([])
   }, [])
 
+  /**
+   * @todo It's very convenient to guarantee a string return,
+   * but we should configure this to gracefully fail creation
+   * when a duplicate custom `id` is set in options
+   */
   const addToast = (content: React.ReactNode, options: AddToastOptions = {}): string => {
     const id = options.id || getId()
     if (!exists(id)) {
       const { type = 'info', ...rest } = options
       const newToast = { content, id, type, ...rest }
-      setToasts((prevToasts) => [...prevToasts, newToast])
+      if (maxToasts && maxToasts < toasts.length + 1) {
+        setToasts((prevToasts) => [...prevToasts, newToast].slice(toasts.length + 1 - maxToasts))
+      } else {
+        setToasts((prevToasts) => [...prevToasts, newToast])
+      }
       return id
     } else {
-      return addToast(content, options)
+      return addToast(content, { ...options, id: getId() })
     }
   }
 
