4444f80be085beef4a1181dca87e2ac7b6c80a6e
Tom Pridham <pridham.tom@gmail.com>
2020-09-28 16:45:14 -0600

    feat: wrap functions in useCallback and use function `setState` to prevent including toasts in deps

--- packages/react-cooked-bread/src/active-toasts-hook.ts
+++ packages/react-cooked-bread/src/active-toasts-hook.ts
@@ -3,65 +3,74 @@ import { useState, useCallback } from 'react'
 import { ActiveToast, Id, AddToastOptions, UpdateToastOptions } from './types'
 import { getId } from './utils'
 
+const checkForId = (id: string) => (t: ActiveToast) => t.id === id
+
 export const useActiveToasts = (maxToasts: number | undefined) => {
   const [toasts, setToasts] = useState<ActiveToast[]>([])
   const hasToasts = !!toasts.length
 
-  const exists = (id: Id) => {
-    if (id && hasToasts) {
-      return !!toasts.filter((t) => t.id === id).length
-    }
-  }
+  const exists = useCallback((id: Id) => toasts.some(checkForId(id)), [toasts])
 
-  const removeToast = (id: Id) => {
-    if (exists(id)) {
-      setToasts((prevToasts) => prevToasts.filter((t) => t.id !== id))
-    }
-  }
+  const removeToast = useCallback(
+    (id: Id) => {
+      if (exists(id)) {
+        setToasts((t) => t.filter((toast) => toast.id !== id))
+      }
+    },
+    [exists]
+  )
 
   const removeAllToasts = useCallback(() => {
     setToasts([])
   }, [])
+
+  const updateToast = useCallback(
+    (id: Id, options: UpdateToastOptions = {}) => {
+      if (exists(id)) {
+        setToasts((t) => {
+          const index = t.findIndex(checkForId(id))
+          const updatedToast = { ...t[index], ...options }
+          t.splice(index, 1, updatedToast)
+          return t
+        })
+      }
+    },
+    [exists]
+  )
 
   /**
    * @todo It's very convenient to guarantee a string return,
    * but we should configure this to gracefully fail creation
    * when a duplicate custom `id` is set in options
    */
-  const addToast = (content: React.ReactNode, options: AddToastOptions = {}): string => {
-    const id = options.id || getId()
-    if (!exists(id)) {
-      const { type = 'info', ...rest } = options
-      const newToast = { content, id, type, ...rest }
-      if (maxToasts && maxToasts < toasts.length + 1) {
-        setToasts((prevToasts) => [...prevToasts, newToast].slice(toasts.length + 1 - maxToasts))
+  const addToast = useCallback(
+    (content: React.ReactNode, options: AddToastOptions = {}): string => {
+      const id = options.id
+      if (id && !exists(id)) {
+        const { type = 'info', ...rest } = options
+        const newToast = { content, id, type, ...rest }
+        setToasts((t) => {
+          if (maxToasts && maxToasts < t.length + 1) {
+            return [...t.slice(1), newToast]
+          } else {
+            return [...t, newToast]
+          }
+        })
+        return id
       } else {
-        setToasts((prevToasts) => [...prevToasts, newToast])
+        const i = getId()
+        return addToast(content, { ...options, id: i })
       }
-      return id
-    } else {
-      return addToast(content, { ...options, id: getId() })
-    }
-  }
-
-  const updateToast = (id: Id, options: UpdateToastOptions = {}) => {
-    if (exists(id)) {
-      const index = toasts.findIndex((t) => t.id === id)
-      const updatedToast = { ...toasts[index], ...options }
-      setToasts((prevToasts) => [
-        ...prevToasts.slice(0, index),
-        updatedToast,
-        ...prevToasts.slice(index + 1),
-      ])
-    }
-  }
+    },
+    [exists, maxToasts]
+  )
 
   return {
-    toasts,
     addToast,
-    removeToast,
-    removeAllToasts,
-    updateToast,
     hasToasts,
+    removeAllToasts,
+    removeToast,
+    toasts,
+    updateToast,
   }
 }
